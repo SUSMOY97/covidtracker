c5c6a072fb1036a15213169091f7a0b502ec8181
Susmoy <susmoymukherjee@gmail.com>
2020-05-12 23:56:57 +0530

    Updates

---
+++ precache-manifest.bf76cb460df620901e17e5c6bdcf09c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ef0929e28c78a079bd90fc10ee104f",
+    "url": "/covidtracker/index.html"
+  },
+  {
+    "revision": "1959dcd4e285c233922e",
+    "url": "/covidtracker/static/css/main.1382f07e.chunk.css"
+  },
+  {
+    "revision": "de3ab18ceb264395524f",
+    "url": "/covidtracker/static/js/2.4521a7b0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidtracker/static/js/2.4521a7b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1959dcd4e285c233922e",
+    "url": "/covidtracker/static/js/main.7381c911.chunk.js"
+  },
+  {
+    "revision": "bf94e0a017409e884242",
+    "url": "/covidtracker/static/js/runtime-main.f24466a6.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidtracker/static/media/image.d7265326.png"
+  }
+]);
